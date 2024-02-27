58ec239186710ae2387b2c7e03c114bde651f300
stas <stanislav.voitkus@outlook.com>
2024-02-28 02:21:32 +0300

    kek

--- rectangle/Hyperbola.py
+++ rectangle/Hyperbola.py
@@ -1,8 +1,8 @@
 def draw_hyperbola(event_1, event_2):
     a = abs(event_2.x - event_1.x) // 2
     b = abs(event_2.y - event_1.y) // 2
-    h = (event_1.x + event_2.x) // 2  # center x-coordinate
-    k = (event_1.y + event_2.y) // 2  # center y-coordinate
+    h = (event_1.x + event_2.x) // 2
+    k = (event_1.y + event_2.y) // 2
     x = 0
     y = b
     d = b * b - a * a * b + a * a / 4
@@ -16,8 +16,8 @@ def draw_hyperbola(event_1, event_2):
             d = d + b * b * (2 * x + 3) + a * a * (-2 * y + 2)
             x = x + 1
             y = y - 1
-        pixels.append((x + h, -y + k))  # first branch
-        pixels.append((-x + abs(event_1.x - event_2.x) + h, y - abs(event_1.y - event_2.y) + k))  # second branch
+        pixels.append((x + h, -y + k))
+        pixels.append((-x + abs(event_1.x - event_2.x) + h, y - abs(event_1.y - event_2.y) + k))
 
     d = b * b * (x + 1) * (x + 1) + a * a * (y - 1) * (y - 1) - a * a * b * b
 
@@ -29,7 +29,7 @@ def draw_hyperbola(event_1, event_2):
         else:
             d = d + a * a * (-2 * y + 3)
             y = y - 1
-        pixels.append((x + h, -y + k))  # first branch
-        pixels.append((-x + abs(event_1.x - event_2.x) + h, y - abs(event_1.y - event_2.y) + k))  # second branch
+        pixels.append((x + h, -y + k))
+        pixels.append((-x + abs(event_1.x - event_2.x) + h, y - abs(event_1.y - event_2.y) + k))
 
     return pixels
